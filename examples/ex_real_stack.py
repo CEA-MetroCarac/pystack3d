f1665fd17fe4c5c77d9ae7473f4249c181cc56e5
patquem <patrick.quemere@cea.fr>
2024-03-20 14:52:55 +0100

    align real example on synthetic example formalism

--- examples/ex_real_stack.py
+++ examples/ex_real_stack.py
@@ -18,8 +18,7 @@ DATA = ASSETS / 'stacks' / 'stack_1'
 
 
 def ex_real_stack(process_steps=None, dirfunc=None, nproc=None,
-                  serial=True, verbosity=True, show_pbar=True,
-                  show_plots=True):
+                  verbosity=True, show_pbar=True, show_plots=True):
     """ Example with real data """
 
     toml_from_metadata = False
@@ -27,30 +26,30 @@ def ex_real_stack(process_steps=None, dirfunc=None, nproc=None,
     with dirfunc() as dirpath:  # open user temp or TemporaryDirectory dirpath
 
         # Initialize the project directory in the temporary folder
-        dirname = init_dir(dirpath, case='real',
-                           copy_params_toml=not toml_from_metadata)
+        dir_proj = init_dir(dirpath, case='real',
+                            copy_params_toml=not toml_from_metadata)
 
         # copy the slices in the project directory
         for channel in ["ESB", "SE2"]:
-            os.makedirs(dirname / channel, exist_ok=True)
+            os.makedirs(dir_proj / channel, exist_ok=True)
             for src in (DATA / channel).glob('*.tif'):
-                shutil.copy(src, dirname / channel)
+                shutil.copy(src, dir_proj / channel)
 
         if toml_from_metadata:
-            shutil.copy(DATA / 'Atlas3D.a3d-setup', dirname)
+            shutil.copy(DATA / 'Atlas3D.a3d-setup', dir_proj)
             fname_toml = ASSETS / 'toml' / 'params_real_stack.toml'
-            params_from_metadata(dirname, fname_toml_ref=fname_toml, save=True)
+            params_from_metadata(dir_proj, fname_toml_ref=fname_toml, save=True)
 
         # processing
-        stack = Stack3d(input_name=dirname)
-        stack.eval(process_steps=process_steps, nproc=nproc, serial=serial,
+        stack = Stack3d(input_name=dir_proj)
+        stack.eval(process_steps=process_steps, nproc=nproc,
                    show_pbar=show_pbar)
 
         # post-processing
         if process_steps is None:
             process_steps = stack.params['process_steps']
         channel = stack.params['channels'][0]
-        dirnames, labels, stats = postpro(process_steps, dirname, channel,
+        dirnames, labels, stats = postpro(process_steps, dir_proj, channel,
                                           verbosity=verbosity)
         if show_plots:
             plot_results(dirnames, labels, stats.min(), stats.max())
@@ -64,16 +63,13 @@ if __name__ == '__main__':
     # DIRFUNC = UserTempDirectory  # use the user temp location
     DIRFUNC = TemporaryDirectory  # use a TemporaryDirectory
     NPROC = 1
-    REGISTRATION = ['registration_calculation', 'registration_transformation']
+    PROCESS_STEPS = [
+        'cropping',
+        'bkg_removal',
+        # 'destriping',
+        # 'registration_calculation', 'registration_transformation',
+        # 'intensity_rescaling',
+        # 'resampling',
+    ]
 
-    # ex_real_stack('cropping', DIRFUNC, NPROC)
-    # ex_real_stack('bkg_removal', DIRFUNC, NPROC)
-    # ex_real_stack('intensity_rescaling', DIRFUNC, NPROC)
-    # ex_real_stack('destriping', DIRFUNC, NPROC)
-    # ex_real_stack(REGISTRATION, DIRFUNC, NPROC)
-    # ex_real_stack('resampling', DIRFUNC, NPROC)
-
-    ex_real_stack(['cropping', 'bkg_removal'], DIRFUNC, NPROC)
-
-    # # Launch all process steps
-    # ex_real_stack(dirfunc=DIRFUNC, nproc=NPROC)
+    ex_real_stack(PROCESS_STEPS, DIRFUNC, NPROC)
