be515ecc6810e9fbb93f4201acd900b063ecd4ac
patquem <patrick.quemere@cea.fr>
2025-06-12 09:38:56 +0200

    replace 'assert' by 'UserWarning'

--- pystack3d/cropping.py
+++ pystack3d/cropping.py
@@ -1,6 +1,7 @@
 """
 Functions related to the cropping processing
 """
+import warnings
 import numpy as np
 from tifffile import TiffFile
 
@@ -66,10 +67,11 @@ def inds_from_area(area, fnames, pid_0, output_dirname):
         imin, imax = shape[1] - area[3], shape[1] - area[2]
 
         if pid_0:
-            msg = '\nERROR!! your area ({}) exceed the image shape ({})'
-            msg += ' according to the {}-direction'
-            assert area[1] <= shape[2], msg.format(area[1], shape[2], 'x')
-            assert area[3] <= shape[1], msg.format(area[3], shape[1], 'y')
+            msg = 'your area ({}) exceed the image shape ({}) according to the {}-direction'
+            if area[1] > shape[2]:
+                warnings.warn(msg.format(area[1], shape[2], 'x'), category=UserWarning)
+            if area[3] > shape[1]:
+                warnings.warn(msg.format(area[3], shape[1], 'y'), category=UserWarning)
             with open(output_dirname / 'outputs' / 'log.txt', 'w') as fid:
                 fid.write(f"Original shape: {shape}")
                 fid.write(f"New shape: {(shape[0], jmax - jmin, imax - imin)}")
