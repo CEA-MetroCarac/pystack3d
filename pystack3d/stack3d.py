4cc175fb835b4c97d2840298d0418ad5413677c6
patquem <patrick.quemere@cea.fr>
2025-06-18 13:15:41 +0200

    minor correction

--- pystack3d/stack3d.py
+++ pystack3d/stack3d.py
@@ -17,7 +17,6 @@ import numpy as np
 import matplotlib.pyplot as plt
 from tifffile import TiffFile, TiffWriter, imwrite
 from tomli import load
-from tomlkit import dumps
 
 from pystack3d.utils import imread_3d_skipping, get_tags, dumps_params, check_cupy
 from pystack3d.utils_multiprocessing import worker_init, step_wrapper, initialize_args
@@ -334,7 +333,7 @@ class Stack3d:
             # 'history' parameter updating and saving
             if serial and self.fname_toml:
                 self.params['history'] = self.params['history'] + [process_step]
-                self.fname_toml.write_text(dumps_params(params), encoding='utf-8')
+                self.fname_toml.write_text(dumps_params(self.params), encoding='utf-8')
 
     def cleanup(self):
         """ Remove the .tif files except the ones related to the last process"""
